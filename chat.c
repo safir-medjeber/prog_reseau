e1193db3cffba36e9a12c9cc5d9b88dee8ad5992
GEDIK marc <gedik@informatique.univ-paris-diderot.fr>
2014-04-16 16:34:54 +0200

    Chat avec soi-meme possible

--- chat.c
+++ chat.c
@@ -1,65 +1,79 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <pthread.h>
 
-void * client(){
+#include <poll.h>
+
+int createSocket(int port, char* addresse){
   struct sockaddr_in addr;
   int sock;
-  char buff[50];
 
   sock = socket(PF_INET, SOCK_STREAM, 0);
   
   bzero(&addr, sizeof(addr));
   
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(60212);
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = inet_addr(addresse);
   
   connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-  
-  for(;;){
-    printf(">");
-    scanf("%s", buff);
-    write(sock, buff, 50);
-  } 
+  perror("client");
+  return sock;
 }
 
-void * serveur (){
-  struct sockaddr_in addr, c;
-  int sock_serv, sock;
-  unsigned int csize;
+int createServerSocket(int port){
+  struct sockaddr_in addr;
+  int sock;
   
-  char buff[50];
-  
-  sock_serv = socket(PF_INET, SOCK_STREAM, 0);
+  sock = socket(PF_INET, SOCK_STREAM, 0);
   
   bzero(&addr, sizeof(addr));
   
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(60212);
+  addr.sin_port = htons(port);
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
   
-  bind(sock_serv, (struct sockaddr *)&addr, sizeof(addr));
+  bind(sock, (struct sockaddr *)&addr, sizeof(addr));
   
-  listen(sock_serv, 0);
+  listen(sock, 0);
+
+  return sock;
+}
+
+void * chat (int portServeur, int portClient){
+  struct sockaddr_in c;
+  int sock_serv, sock, s;
+  unsigned int csize;
+  struct pollfd polls[2]; 
+  
+  char buff[50];
+
+  sock_serv = createServerSocket(portServeur);
+  s = createSocket(portClient, "127.0.0.1");
+
   csize = sizeof(c);
   
   sock = accept(sock_serv, (struct sockaddr *)&c, &csize);
+
+  polls[0].fd = 0;
+  polls[0].events = POLLIN;
   
+  polls[1].fd = sock;
+  polls[1].events = POLLIN;
+
   while(1){
-    read(sock, buff, 50);
-    printf("recu>%s\n", buff);
+    poll(polls, 2, -1);
+    if(polls[0].revents == POLLIN){
+      fgets(buff, 50, stdin);
+      write(s, buff, 50);
+    }
+    if(polls[1].revents == POLLIN){
+      read(sock, buff, 50);
+      printf("recu>%s", buff);
+    }
   }
 }
 
 int main(int argc, int ** argv){
-  pthread_t thread_serv, thread_client;
-  
-  pthread_create(&thread_serv, NULL, serveur, NULL);
-  pthread_create(&thread_client, NULL, client, NULL);
-  
-  pthread_join(thread_serv, NULL);
-  pthread_join(thread_client, NULL);
-  
+  chat(atoi(argv[1]), atoi(argv[2]));
 }
