62dae27d4f151a996eaa2f24b039c55a63fd30ae
MEDJEBER safir <medjeber@lucien.informatique.univ-paris-diderot.fr>
2014-04-30 16:05:15 +0200

    read write byte

--- java/ThreadWRI.java
+++ java/ThreadWRI.java
@@ -1,5 +1,6 @@
 import  java.net.*;
 import java.io.*;
+import java.lang.*;
 import java.util.Scanner;
 
 
@@ -13,6 +14,25 @@ public class ThreadWRI implements Runnable{
 	this.s=s;
     }
 
+
+
+    public  void myWritter(String msg, Socket s){
+	try{
+	    
+	    msg= "MSG " + "005 "  + msg;
+	    byte[] tab =  msg.getBytes();
+	    OutputStream oos = new ObjectOutputStream(s.getOutputStream());
+	    oos.write(tab);
+	    oos.flush();
+	}
+	catch(Exception e){
+	    System.out.println("erreur writter");
+	}
+
+    }
+
+
+
     public void run(){
 	try{
 	    String msg;
@@ -21,15 +41,23 @@ public class ThreadWRI implements Runnable{
 	    while(true){
 		Scanner sc = new Scanner(System.in);
 		msg = sc.nextLine();
-		//  System.out.println("j'envoi ça :" + msg);
-		pw.println(msg);
-		pw.flush();	
+			myWritter(msg,s);
+
+		//	System.out.println("j'envoi ça :" + msg);
+		//	pw.print(msg);
+		//	pw.flush();
 	    }
 	}
 	catch (IOException e) {
 	    System.out.println("threadWRI \n"+ e);
 	}
     }
+
+
+
+
 }
+
+
 
 
